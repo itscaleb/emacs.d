61b1c4e4875bee9f0d26be25e2d088ae4551f730
Caleb Gossler <caleb.gossler@gmail.com>
2017-03-09 10:47:04 -0800

    Trying to manage packages with Nix
    The Nix package manager seems to offer a saner way to manage emacs
    packages. I'm moving all of the package management stuff out of my
    emacs config, and into a Nix expressing using emacsWithPackages. I'll
    have the nix expression checked in soon.

--- init.el
+++ init.el
@@ -2,6 +2,12 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+(require 'package)
+
+;; optional. makes unpure packages archives unavailable
+(setf package-archives nil)
+
+(setf package-enable-at-startup nil)
 (package-initialize)
 
 (org-babel-load-file "~/.emacs.d/readme.org")
