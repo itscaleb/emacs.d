8bd4e522da0ba7d6606d0da74ae67974e62c83e0
Caleb Gossler <caleb.gossler@gmail.com>
2017-03-09 18:18:21 -0800

    Using Nix to manage Emacs Packages
    I'm experimenting with using Nix to manage my packages and taking all
    package related setup stuff out of my emacs config. It seems to be
    working pretty well.
    
    This has one emacs nix package setup for my nixos linux laptop, and one
    for my work mac laptop that uses the macport emacs.

---
+++ emacs.nix
@@ -0,0 +1,37 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  myEmacs = pkgs.lib.overrideDerivation (pkgs.emacs25.override {
+    withGTK3 = true;
+    withGTK2 = false;
+    withXwidgets = true;
+  }) (attrs: {});
+
+  linuxEmacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
+
+  macEmacsWithPackages = (pkgs.emacsPackagesNgGen pkgs.emacs25Macport).emacsWithPackages;
+
+  myEmacsPackages = epkgs: (with epkgs.melpaPackages; [
+    bbdb
+    company
+    emms
+    exec-path-from-shell
+    helm
+    jabber
+    magit
+    multiple-cursors
+    nix-mode
+    paredit-everywhere
+    pdf-tools
+    restclient
+    spaceline
+    zenburn-theme
+  ]) ++ (with epkgs.orgPackages; [
+    org-plus-contrib
+  ]);
+
+# install one of these with "nix-env -f /path/to/emacs.nix -iA emacsLinux"
+in {
+  emacsLinux = linuxEmacsWithPackages (myEmacsPackages);
+  emacsMac = macEmacsWithPackages (myEmacsPackages);
+}
